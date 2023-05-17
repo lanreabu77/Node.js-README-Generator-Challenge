9dfc41788dc3dd22201920f2b0151a85d59809ab
Lanreabu77 <lanreabu@gmail.com>
2023-05-17 17:31:26 -0500

    added index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,52 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a brief description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions for your project?',
+    
+    },
+    {
+        type: 'input',
+        name: 'screenshot',
+        message: 'Would you like to include a screenshot of your project?'
+    },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your github username:',
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email:',
+    },
+];
+
+inquirer.prompt([
+  // Prompt the user for input
+]).then((answers) => {
+  // Use the user's input to generate the README.md file
+  const readmeContent = `# ${answers.title}\n\n${answers.description}\n\n${answers.installation}\n\n${answers.screenshot}`;
+
+  // Write the generated README.md file to disk
+  fs.writeFileSync('README.md', readmeContent);
+});
 
 // Function call to initialize app
 init();
